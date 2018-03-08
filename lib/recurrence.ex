d37cb354bf61d0fe7bf59cd98be7f86357e67f8e
Wojtek Mach <wojtek@wojtekmach.pl>
2018-03-08 22:52:41 +0100

    Cleanup

--- lib/recurrence.ex
+++ lib/recurrence.ex
@@ -1,18 +1,2 @@
 defmodule Recurrence do
-  @moduledoc """
-  Documentation for Recurrence.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Recurrence.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/recurrence_test.exs
+++ test/recurrence_test.exs
@@ -1,8 +1,3 @@
 defmodule RecurrenceTest do
-  use ExUnit.Case
-  doctest Recurrence
-
-  test "greets the world" do
-    assert Recurrence.hello() == :world
-  end
+  use ExUnit.Case, async: true
 end
