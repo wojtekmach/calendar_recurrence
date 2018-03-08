5fd0ffc98eb2bfb68a107726d69df9f9cc6db980
Wojtek Mach <wojtek@wojtekmach.pl>
2018-03-08 22:51:25 +0100

    Initial commit

---
+++ lib/recurrence.ex
@@ -0,0 +1,18 @@
+defmodule Recurrence do
+  @moduledoc """
+  Documentation for Recurrence.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Recurrence.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/recurrence_test.exs
@@ -0,0 +1,8 @@
+defmodule RecurrenceTest do
+  use ExUnit.Case
+  doctest Recurrence
+
+  test "greets the world" do
+    assert Recurrence.hello() == :world
+  end
+end
