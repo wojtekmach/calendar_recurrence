a0d18609cbddd5de002da68810ebec09c070c16c
Kevin Schweikert <k.schweikert@im-en.com>
2024-05-09 11:01:07 +0200

    add github ci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CalendarRecurrence.MixProject do
     [
       app: :calendar_recurrence,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
